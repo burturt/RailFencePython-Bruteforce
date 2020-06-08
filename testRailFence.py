49b3cb9d05cbc55c5fb4f8409984d94d1d3f61a1
burturt <31748545+burturt@users.noreply.github.com>
2020-06-08 16:29:55 -0700

    Update testRailFence.py

--- testRailFence.py
+++ testRailFence.py
@@ -1,6 +1,15 @@
-#!/usr/bin/python
-
 from railFence import decryptRailFence
 
-print decryptRailFence("WECRLTEERDSOEEFEAOCAIVDEN", 3, 0); #(reference: http://en.wikipedia.org/wiki/Rail_Fence_Cipher)
-print decryptRailFence("WWHLIAATTEHSDITETH", 7, 0); #(reference: http://i.imgur.com/kWGmM.gif)
+ciphertext = str(raw_input("Send the cipher text: "))
+ciphertext = ciphertext.replace(" ", "%")
+print "Brute forcing all rails and offsets:"
+
+rails = range(2,10)
+offsets = range(0,10)
+print(rails)
+print(offsets)
+for i in rails:
+    for j in offsets:
+        tempciphertext = " " * j + ciphertext
+        print decryptRailFence(tempciphertext, i,0)
+
