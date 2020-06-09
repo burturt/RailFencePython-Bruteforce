75390e0efbaa3f8957cb065b28a425a090c09540
burturt <31748545+burturt@users.noreply.github.com>
2020-06-08 17:29:59 -0700

    Update testRailFence.py
    fixing borken code

--- testRailFence.py
+++ testRailFence.py
@@ -1,15 +1,26 @@
 from railFence import decryptRailFence
+import sys
+# CHANGE THIS
+# THE LAST NUMBER DOES NOT GET USED
+rails = range(2,11)
+offsets = range(0,11)
 
-ciphertext = str(raw_input("Send the cipher text: "))
+# DONT CHANGE THIS
+
+if len(sys.argv) != 2:
+    print("Usage: python " + sys.argv[0] + " [ciphertext]")
+    print("You may need to use quotes around the ciphertext if it contains spaces")
+    exit(1)
+ciphertext = sys.argv[1]
+print("-----------------------------")
 ciphertext = ciphertext.replace(" ", "%")
 print "Brute forcing all rails and offsets:"
 
-rails = range(2,10)
-offsets = range(0,10)
+
 print(rails)
 print(offsets)
 for i in rails:
     for j in offsets:
-        tempciphertext = " " * j + ciphertext
-        print decryptRailFence(tempciphertext, i,0)
+        tempciphertext = "*" * j + ciphertext
+        print decryptRailFence(tempciphertext, i, 0).replace("*", "").replace("%", " ")
 
